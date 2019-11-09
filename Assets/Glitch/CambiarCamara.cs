2e7cd242b21dfb89a7d779c0846e6d82b6aa9559
Juan David Ramirez Mendoza <juandavidramirezmendoza@hotmail.com>
2019-11-09 18:54:37 -0500

    Efecto de distorsión añadido

---
+++ Assets/Glitch/CambiarCamara.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class CambiarCamara : MonoBehaviour{
+
+    public Camera camera1;
+    public Camera camera2;
+
+    void Start(){
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+
+    void OnTriggerEnter(){
+        camera1.enabled = false;
+        camera2.enabled = true;
+    }
+
+    void OnTriggerExit(){
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+}
