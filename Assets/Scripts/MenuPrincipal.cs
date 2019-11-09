cbfca854349701903697fb735a04ac21ebfe3052
Juan David Ramirez Mendoza <jcramirez@asesoftware.com>
2019-11-09 04:21:29 -0500

    Main Menu Added

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void jugar(){
+        Console.WriteLine("Jugar");
+        SceneManager.LoadScene("StartScene");
+
+    }
+
+    public void salir(){
+        Console.WriteLine("Salir");
+    }
+}
