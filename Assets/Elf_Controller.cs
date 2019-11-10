34cb279ef148a8fabf7f3c93a429677a3519887b
Bryan <bryans.pinzonc@gmail.com>
2019-11-10 14:48:58 -0500

    Sound effects were added

--- Assets/Elf_Controller.cs
+++ Assets/Elf_Controller.cs
@@ -9,15 +9,22 @@ public class Elf_Controller : MonoBehaviour {
     public ParticleSystem poder;
     public GameObject canvas;
     private Text textoPoder;
-
+    public Text sensacionExtraña;
+    public Text finalGameText;
+    private int showFirstText = 1;
+    private AudioSource audioSource;
+    public AudioClip powerSound;
+    public AudioClip impactSound;
 
     // Start is called before the first frame update
     void Start () {
         anim = GetComponent<Animator> ();
-        textoPoder = canvas.GetComponentInChildren<Text>();
+        audioSource = GetComponent<AudioSource> ();
+        textoPoder = canvas.GetComponentInChildren<Text> ();
         textoPoder.text = "";
-        LanzarPoder();
-        showPowerTextFeedback();
+        sensacionExtraña.text = "";
+        finalGameText.text = "";
+        LanzarPoder ();
     }
 
     // Update is called once per frame
@@ -27,13 +34,20 @@ public class Elf_Controller : MonoBehaviour {
         }
     }
 
-    public void showPowerTextFeedback(){
-        textoPoder.text = "¡Presiona la tecla Ctrl Izq para lanzar poderes!";
-        StartCoroutine(hidePowerText());
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.CompareTag ("FinalGameCollider")) {
+            finalGameText.text = "Nunca llegué a imaginar que el fin del mundo lo causarían seres fuera de este planeta";
+            audioSource.PlayOneShot (impactSound, 0.5f);
+        }
     }
 
-    public IEnumerator hidePowerText(){
-        yield return new WaitForSecondsRealtime(7.0f);
+    public void showPowerTextFeedback () {
+        textoPoder.text = "¡Presiona la tecla Ctrl Izq para lanzar poderes!";
+        StartCoroutine (hidePowerText ());
+    }
+
+    public IEnumerator hidePowerText () {
+        yield return new WaitForSecondsRealtime (7.0f);
         textoPoder.text = "";
     }
 
@@ -42,15 +56,25 @@ public class Elf_Controller : MonoBehaviour {
     }
 
     public IEnumerator LanzaPoderCoroutine () {
+
+        if (showFirstText == 1) {
+            showFirstText++;
+            sensacionExtraña.text = "¿Qué sucedió? Nunca me había sentido de esta forma.";
+            yield return new WaitForSecondsRealtime (5.0f);
+            showPowerTextFeedback ();
+            sensacionExtraña.text = "";
+        }
+
         anim.SetBool ("LanzandoPoder", true);
         yield return new WaitForSecondsRealtime (2.0f);
-        poder.Play();
-        StartCoroutine(DetenerPoder());
+        poder.Play ();
+        audioSource.PlayOneShot (powerSound, 0.5f);
+        StartCoroutine (DetenerPoder ());
         anim.SetBool ("LanzandoPoder", false);
     }
 
-    public IEnumerator DetenerPoder(){
-        yield return new WaitForSecondsRealtime(1.0f);
-        poder.Stop();
+    public IEnumerator DetenerPoder () {
+        yield return new WaitForSecondsRealtime (1.0f);
+        poder.Stop ();
     }
 }
